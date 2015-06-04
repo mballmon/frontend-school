f1706991187b689dd271ee633476e36698cbc762
Matt Allmon <allmon.matt@gmail.com>
2015-06-03 20:57:29 -0500

    moved cs and js to individual files

---
+++ script.js
@@ -0,0 +1,5 @@
+ document.getElementByID("mylink").style.fontWeight = 
+"bold"
+        console.log("I'm logging things!")
+      document.getElementById("myspan").onclick = function(){alert('testing testing 1, 2, 3');
+                                                            };
